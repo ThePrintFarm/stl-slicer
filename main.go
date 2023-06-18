94f8440c55bdbc6f48cd940745e235eae9edfe12
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2023-06-18 11:41:35 -0700

    added stubs for prusa/cura endpoints

--- main.go
+++ main.go
@@ -4,39 +4,58 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
-const keyServerAddr = "serverAddr"
+func dbg(s string) {
+	fmt.Printf("\033[1;32mDBG\033[0m: \033[1;37m%s\033[0m\n", s)
+}
 
-func getStl(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("DBG: got /stl request")
-
-	myName := r.PostFormValue("file")
-	fmt.Printf("DBG: %s\n", myName)
-	if myName == "" {
-		w.Header().Set("x-missing-field", "file")
-		w.WriteHeader(http.StatusBadRequest)
+func getStlGoSlice(w http.ResponseWriter, r *http.Request) {
+	// parse the multipart form data
+	err := r.ParseMultipartForm(32 << 20)
+	if err != nil {
+		dbg("Error parsing multipart form")
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Printf("DBG: %+v\n", r)
-	io.WriteString(w, fmt.Sprintf("Hello, %+v!\n", r))
+
+	f, h, err := r.FormFile("file")
+	if err != nil {
+		dbg("Error getting file from form")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer f.Close()
+
+	dst, err := os.Create(h.Filename)
+	if err != nil {
+		dbg("Error creating file")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	s, err := io.Copy(dst, f)
+	if err != nil {
+		dbg("Error copying file")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	dbg(fmt.Sprintf("Wrote %d bytes to %s", s, h.Filename))
+
+	dbg(fmt.Sprintf("%+v\n", r))
+	fmt.Fprintln(w, "Upload successful")
 }
 
-func getStlOld(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		fmt.Fprintln(w, "Failed to parse form data")
-		panic(err)
-	}
-	fmt.Printf("DEBUG1: %+v\n", r.Form)
-	x := r.Form.Get("file")
-	fmt.Printf("DEBUG2: %+v\n", r)
-	fmt.Printf("DEBUG3: %s\n", x)
-	fmt.Fprintln(w, "Form data parsed.")
-}
+func getStlCura(w http.ResponseWriter, r *http.Request) {}
+
+func getStlPrusa(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
-	http.HandleFunc("/stl", getStl)
+	http.HandleFunc("/stl/goslice", getStlGoSlice)
+	http.HandleFunc("/stl/cura", getStlCura)
+	http.HandleFunc("/stl/prusa", getStlPrusa)
 
 	err := http.ListenAndServe("0.0.0.0:9999", nil)
 	if err != nil {
